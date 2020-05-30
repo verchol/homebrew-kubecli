eec5496913ad4c646be14e5d76119d148223c167
Oleg Verhovsky <oleg@codefresh.io>
2020-05-30 12:45:18 +0300

    Brew formula update for kubecli version v1.0.10

--- Formula/kubecli.rb
+++ Formula/kubecli.rb
@@ -2,9 +2,18 @@
 class Kubecli < Formula
   desc "kubecli --help"
   homepage "http://docs.codefresh.io"
-  url "https://github.com/verchol/kubecli/releases/download/v1.0.9/kubecli_1.0.9_Darwin_x86_64.tar.gz"
-  version "1.0.9"
-  sha256 "6224173ab6f62837e0874725d1c6e8716c345dc026cdbf5b62698d470048f997"
+  version "1.0.10"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/verchol/kubecli/releases/download/v1.0.10/kubecli_1.0.10_Darwin_x86_64.tar.gz"
+    sha256 "d0922148c8c18c75b1337e243c31bc0483b0414552d5cc332dcf770c5e997bd4"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/verchol/kubecli/releases/download/v1.0.10/kubecli_1.0.10_Linux_x86_64.tar.gz"
+      sha256 "9026a9fa797b24b46fd0ebeb36f04b1f97d8efac0dbf89d1b2278a386b5ff458"
+    end
+  end
 
   def install
     bin.install "kubecli"
